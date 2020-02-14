80030b6a653e2b244d53932163bdc3941a3085f8
ZMelisko <zmelisko@gmail.com>
2020-02-13 23:31:36 -0800

    done

--- js/main.js
+++ js/main.js
@@ -2,11 +2,28 @@
 $(function () {
   $(document).scroll(function () {
     var $nav = $(".navbar-fixed-top");
-    if ($(document).width() > 800) {
+    if ($(document).width() >= 1025) {
       $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
     }
+
   });
+});
+
+
+$(function () {
   $('#burger').click(function() {
-    $('.navbar-fixed-top').show('nav-links')
+    $('nav').toggleClass('show');
+    $(this).toggleClass('active');
+
+    console.log('worked')
   });
+
+});
+
+$(function () {
+  $('#nav-links p').click(function() {
+    $('nav ul').toggleClass('show');
+    console.log('worked')
+  });
+
 });
